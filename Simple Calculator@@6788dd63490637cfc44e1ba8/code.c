70f141663e665edfccc03c240bfdcb6d9e2c0888
SUJAL <sujalhooda6@gmail.com>
2025-02-18 01:47:19 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -31,7 +31,7 @@ int main() {
                 result = num1 / num2;
                 printf("%d", result);
             } else {
-                printf("Error\n");
+                printf("error\n");
             }
             break;
 
